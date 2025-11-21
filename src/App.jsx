1eb8a3d7c4dbb9d411aff69c6c255b7227ee0788
shohan89 <your-maffin.killer007@gmail.com>
2025-11-21 16:36:18 +0600

    Tailwind css v4 custom theme issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import Header from "./components/Header"
+import Header from "./components/Header";
+import Sidebar from './components/Sidebar';
+import './index.css';
+;
 
 function App() {
-
   return (
     <>
       <Header />
+      <main>
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          {/* <!-- Sidebar --> */}
+          <Sidebar />
+
+          {/* <!-- Content --> */}
+          
+        </div>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
