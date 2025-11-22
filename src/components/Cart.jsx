c970c9502d0d2605155d1cd9254d2410cf1bf365
shohan89 <your-maffin.killer007@gmail.com>
2025-11-23 03:13:43 +0600

    add cart open and close functionality

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,9 +1,9 @@
 
-export default function Cart() {
+export default function Cart({ onClose }) {
   return (
     <div className="fixed top-0 left-0 w-screen h-screen z-50 bg-black/60 backdrop-blur-sm">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 w-full max-w-[420px] sm:max-w-[600px] lg:max-w-[790px] p-4 max-h-[90vh] overflow-auto">
-        <div className="bg-white shadow-md dark:bg-[#12141D] rounded-2xl overflow-hidden p-5 md:p-9">
+        <div className="bg-white shadow-md rounded-2xl overflow-hidden p-5 md:p-9">
           <h2 className="text-2xl lg:text-[30px] mb-10 font-bold">Your Carts</h2>
           <div className="space-y-8 lg:space-y-12 max-h-[450px] overflow-auto mb-10 lg:mb-14">
             <div className="grid grid-cols-[1fr_auto] gap-4">
@@ -65,8 +65,9 @@ export default function Cart() {
               <span>Checkout</span>
             </a>
             <a
-              className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] dark:text-gray-200 font-semibold text-sm"
+              className="border border-[#74766F] rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#6F6F6F] font-semibold text-sm"
               href="#"
+              onClick={onClose}
             >
               Cancel
             </a>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,59 @@
-import Moon from '../assets/icons/moon.svg'
-import Logo from '../assets/logo.svg'
-import Ring from '../assets/ring.svg'
-import ShoppingCart from '../assets/shopping-cart.svg'
+import { useState } from "react";
+import Moon from "../assets/icons/moon.svg";
+import Logo from "../assets/logo.svg";
+import Ring from "../assets/ring.svg";
+import ShoppingCart from "../assets/shopping-cart.svg";
+import Cart from "./Cart";
 
 export default function Header() {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+
+  // cart open handler
+  function handleCartOpen() {
+	setIsCartOpen(true);
+  }
+
+  // cart close handler
+  function handleCartClose() {
+	setIsCartOpen(false);
+  }
   return (
-    <header className='text-center px-4'>
-		<nav className="container flex items-center justify-between space-x-10 py-6">
-			<a href="index.html">
-				<img src={Logo} width="139" height="26" alt="" />
-			</a>
-			<ul className="flex items-center space-x-5">
-				<li>
-					<a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-						<img src={Ring} width="24" height="24" alt="" />
-					</a>
-				</li>
-				<li>
-					<a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-						<img src={Moon} width="24" height="24" alt="" />
-					</a>
-				</li>
-				<li>
-					<a className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block" href="#">
-						<img src={ShoppingCart} width="24" height="24" alt="" />
-					</a>
-				</li>
-			</ul>
-		</nav>
-	</header>
-  )
+    <>
+      {isCartOpen && <Cart onClose={handleCartClose}/>}
+      <header className="text-center px-4">
+        <nav className="container flex items-center justify-between space-x-10 py-6">
+          <a href="index.html">
+            <img src={Logo} width="139" height="26" alt="" />
+          </a>
+          <ul className="flex items-center space-x-5">
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Ring} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+              >
+                <img src={Moon} width="24" height="24" alt="" />
+              </a>
+            </li>
+            <li>
+              <a
+                className="bg-primary/20 dark:bg-primary/[7%] rounded-lg backdrop-blur-[2px] p-1 inline-block"
+                href="#"
+				onClick={handleCartOpen}
+              >
+                <img src={ShoppingCart} width="24" height="24" alt="" />
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
