d1466fbc0200c471c376cc5e7906e8835881b5ac
shohan89 <your-maffin.killer007@gmail.com>
2025-11-23 02:25:46 +0600

    create modal for showing movie details

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,28 +1,45 @@
-import { getImgUrl } from '../utils/cine-utility'
-import Rating from './Rating'
+import { useState } from "react";
+import { getImgUrl } from "../utils/cine-utility";
+import MovieDetailsModal from "./MovieDetailsModal";
+import Rating from "./Rating";
 
 export default function MovieCard({ movie }) {
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    // Modal open handler
+    function handleModalOpen() {
+        setIsModalOpen(true);
+    }
+
+    // Modal close handler
+    function handleModalClose() {
+        setIsModalOpen(false);
+    }
   return (
-    <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
-            <img
-              className="w-full object-cover"
-              src={getImgUrl(movie.cover)}
-              alt={movie.title}
-            />
-            <figcaption className="pt-4">
-              <h3 className="text-xl mb-1">{movie.title}</h3>
-              <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
-              <div className="flex items-center space-x-1 mb-5">
-                <Rating value={movie.rating}/>
-              </div>
-              <a
-                className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
-                href="#"
-              >
-                <img src="./assets/tag.svg" alt="" />
-                <span>${movie.price} | Add to Cart</span>
-              </a>
-            </figcaption>
-          </figure>
-  )
+    <>
+      {isModalOpen && (<MovieDetailsModal onClose={handleModalClose}/>)}
+      <figure className="p-4 border border-black/10 shadow-sm dark:border-white/10 rounded-xl">
+        <a onClick={handleModalOpen} href="#">
+        <img
+          className="w-full object-cover"
+          src={getImgUrl(movie.cover)}
+          alt={movie.title}
+        />
+        <figcaption className="pt-4">
+          <h3 className="text-xl mb-1">{movie.title}</h3>
+          <p className="text-[#575A6E] text-sm mb-2">{movie.genre}</p>
+          <div className="flex items-center space-x-1 mb-5">
+            <Rating value={movie.rating} />
+          </div>
+          <a
+            className="bg-primary rounded-lg py-2 px-5 flex items-center justify-center gap-2 text-[#171923] font-semibold text-sm"
+            href="#"
+          >
+            <img src="./assets/tag.svg" alt="" />
+            <span>${movie.price} | Add to Cart</span>
+          </a>
+        </figcaption>
+        </a>
+      </figure>
+    </>
+  );
 }
